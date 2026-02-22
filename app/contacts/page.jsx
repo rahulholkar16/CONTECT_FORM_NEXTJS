a8700a7a4a64d625c1bd5171caa512cbdf2b6db4
Rahul Pal <usefulltech1819@gmail.com>
2026-02-22 23:48:03 +0530

    Add Contact List Componats

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -1,3 +1,4 @@
+import ContactList from '@/components/ContactList';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import React from 'react'
@@ -5,7 +6,7 @@ import React from 'react'
 const Contacts = () => {
   return (
     <div className="min-h-screen py-8 px-4">
-        <div className="container mx-auto max-w-4xl bg-red-700">
+        <div className="container mx-auto max-w-4xl">
           <div className='mb-8'>
             <Link href={'/'}>
               <Button variant='outline' size='sm' className={'mb-4 bg-transparent'}>Back to form</Button>
